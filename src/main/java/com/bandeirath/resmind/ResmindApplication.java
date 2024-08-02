fc0811992606e6d455ae2ab67a795d859ed32204
bandeira-th <trabalho.bandeira@gmail.com>
2024-08-02 15:54:22 -0300

    initial commit

---
+++ src/main/java/com/bandeirath/resmind/ResmindApplication.java
@@ -0,0 +1,13 @@
+package com.bandeirath.resmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResmindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResmindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandeirath/resmind/ResmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandeirath.resmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
